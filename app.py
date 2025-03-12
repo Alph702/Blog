6c07802e1a21e5e0def09460b17596d9e2fffa2f
Alph702 <147134651+Alph702@users.noreply.github.com>
2025-03-12 11:30:43 +0500

    Format timestamps in post filtering and viewing for improved readability

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def filter_posts():
     conn = sqlite3.connect('blog.db')
     c = conn.cursor()
     c.execute(query, params)
-    posts = [(post[0], post[1], Markup(post[2]), post[3], post[4]) for post in c.fetchall()]
+    posts = [(post[0], post[1], Markup(post[2]), post[3], datetime.strptime(post[4], "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d %I:%M %p")) for post in c.fetchall()]
 
     # Get available years, months, and days dynamically from the posts
     c.execute("SELECT DISTINCT strftime('%Y', timestamp) as year FROM posts ORDER BY year DESC")
@@ -197,7 +197,7 @@ def view_post(post_id):
     conn.close()
 
     if post:
-        formatted_timestamp = post[4]
+        formatted_timestamp = datetime.strptime(post[4], "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d %I:%M %p")
         if post[3]:
             Image = str(post[3]).replace("static/uploads/", "")
         else:
